3bd584f0267447b005de9f5b0fb4dca74583f25f
Mohit <131031018+mohitjangra117@users.noreply.github.com>
2025-01-24 08:29:26 +0000

    Inline_functions

---
+++ inline_function.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+inline int sum(int a, int b){             //////////////////
+    return a+b;
+}
+
+
+int main(){
+
+cout<<"The sum of a+b is "<<sum(10,20);          //  the inline function will puth a+b directly on the call
+
+
+
+}
